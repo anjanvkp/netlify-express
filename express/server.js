b3a1b09ce9bcc0805141ee5db2aa6274eb2dfd91
anjanvkp <anjanvkp@gmail.com>
2020-07-26 00:57:30 +0530

    remove /

--- express/server.js
+++ express/server.js
@@ -17,7 +17,7 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
 app.use('/home', require('./controller/home'));
 
 module.exports = app;
