cdf0c3211ec74bb30aa560f5f718471a5034d28d
anjanvkp <anjanvkp@gmail.com>
2020-07-26 01:06:35 +0530

    2

--- express/server.js
+++ express/server.js
@@ -17,7 +17,7 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/home', require('./controller/home'));
+app.use('/.netlify/functions/server/home', require('./controller/home'));
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
