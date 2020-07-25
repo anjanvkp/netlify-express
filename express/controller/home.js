13030a3532094a4a87f72461a2189bd8b9f77d15
anjanvkp <anjanvkp@gmail.com>
2020-07-26 00:48:41 +0530

    add home

---
+++ express/controller/home.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Home Page</h1>');
+    res.end();
+  });
--- express/server.js
+++ express/server.js
@@ -12,11 +12,13 @@ router.get('/', (req, res) => {
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/home', require('./controller/home'));
 
 module.exports = app;
 module.exports.handler = serverless(app);
