6a77f30f09265027a38f6f4d4b7a04cfc37667c2
anjanvkp <anjanvkp@gmail.com>
2020-08-02 01:19:09 +0530

    rest 8

--- src/api.js
+++ src/api.js
@@ -1,16 +1,16 @@
-
-const express = require('express');
-const serverless = require('serverless-http');
+const express = require("express");
+const serverless = require("serverless-http");
 
 const app = express();
-
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json({"hello" : "anjan"});
+router.get("/", (req, res) => {
+  res.json({
+    hello: "hi!"
+  });
 });
 
+app.use(`/.netlify/functions/api`, router);
 
-
-app.use('/.netlify/functions/api', router);
+module.exports = app;
 module.exports.handler = serverless(app);
