831ffaba373e1bcc5ec5e1dd669977fd134c69ea
anjanvkp <anjanvkp@gmail.com>
2020-08-01 19:07:11 +0530

    rest-1

--- express/server.js
+++ express/server.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
-const app = express();
 const bodyParser = require('body-parser');
+
+const app = express();
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
@@ -42,7 +43,6 @@ router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
