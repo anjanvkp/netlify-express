0228ed70639b626fb0badbb4df19b043eb43c8a3
anjanvkp <anjanvkp@gmail.com>
2020-08-02 00:40:37 +0530

    rest 3

--- express/server.js
+++ express/server.js
@@ -9,42 +9,8 @@ const app = express();
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
 
-const db = require("./app/models");
-const Role = db.role;
-
-db.mongoose
-  .connect(`mongodb://anjan:indian@anjan-shard-00-00-01ifl.azure.mongodb.net:27017,anjan-shard-00-01-01ifl.azure.mongodb.net:27017,anjan-shard-00-02-01ifl.azure.mongodb.net:27017/NETLIFY?ssl=true&replicaSet=ANJAN-shard-0&authSource=admin&retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Successfully connect to MongoDB.");
-  })
-  .catch(err => {
-    console.error("Connection error", err);
-    process.exit();
-  });
-
-require("./app/routes/auth.routes")(app);
-require("./app/routes/user.routes")(app);
-require("./app/routes/notes.routes")(app);
-require("./app/routes/exp.route")(app);
-
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js! ANJAN PAUL</h1>');
-  res.end();
+app.use('/', (req, res) => {
+  res.send("Hello");
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-
-router.post('/', (req, res) => res.json({ postBody: req.body }));
-
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-
-
-module.exports = app;
 module.exports.handler = serverless(app);
